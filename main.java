eb1c89f9cf5d97f1c7b3f370a8658a0bcf51561f
Oscar <oscar.anton.alvarez@gmail.com>
2023-04-30 14:51:42 +0200

    Files

--- main.java
+++ main.java
@@ -1,12 +1,42 @@
-import oscar.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class main {
     public static void main(String[] args) {
-        Color c = Color.RED;
+        Path fileSource = Paths.get("newFile.truc");
+        Path machinDir = Paths.get("machin");
+        Path fileDestination = machinDir.resolve("newFile.newTruc");
 
-        System.out.println(c);
+        try {
+            Files.move(fileSource, fileDestination);
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+        }
 
-        for (Color col : Color.values())
-            System.out.println(col);
+        Path f = Paths.get("exemple.txt");
+        Charset c = Charset.forName("UTF-8");
+
+        StandardOpenOption mode = StandardOpenOption.DELETE_ON_CLOSE;
+
+        try {
+            BufferedWriter bfw = Files.newBufferedWriter(f, c, mode);
+            BufferedReader bfr = Files.newBufferedReader(f);
+            String s = "Hello World";
+
+            bfw.write(s, 2, 5);
+
+            System.out.println(bfr.readLine());
+
+            bfr.close();
+            bfw.close();
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+        }
     }
 }
