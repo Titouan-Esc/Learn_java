e4cf493687c24b4b40f6b70db7473b03e75dfad2
Oscar <oscar.anton.alvarez@gmail.com>
2023-06-15 15:54:54 +0200

    databaseé

--- main.java
+++ main.java
@@ -1,59 +1,33 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.sql.*;
 
-public class Main {
+public class main {
 
     public static void main(String []args) {
-//        ExecutorService ex = Executors.newSingleThreadExecutor();
-//        ExecutorService ex = Executors.newFixedThreadPool(4);
-        ExecutorService ex = Executors.newCachedThreadPool();
 
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "postgres";
 
-        Runnable task1 = () -> {
+        try {
 
-            for (int i = 0; i < 5; i++) {
-                System.out.println(i);
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {}
+            ResultSet res = stmt.executeQuery("SELECT * FROM \"user\" ");
+
+            while (res.next()) {
+                String name = res.getString("name");
+                int age = res.getInt("age");
+                System.out.println(name + age);
             }
-        };
 
-        Runnable task2 = () -> {
+            conn.close();
+            stmt.close();
+            res.close();
 
-            for (int i = 5; i < 9; i++) {
-                System.out.println(i);
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {}
-            }
-        };
-
-        Future<String> fut1 = ex.submit(task1, "Je suis Jean et j'ai fini");
-        Future<String> fut2 = ex.submit(task2, "Je suis Enculé et j'ai fini");
-
-        while (!fut1.isDone() || !fut2.isDone()) {
-            System.out.println("On attend...");
-
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {}
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-
-        if (fut1.isDone()) {
-            try {
-                System.out.println(fut1.get());
-            } catch (Exception e) {}
-        } else if (fut2.isDone()) {
-            try {
-                System.out.println(fut2.get());
-            } catch (Exception e) {}
-        }
-
-        ex.shutdown();
     }
     
 }
